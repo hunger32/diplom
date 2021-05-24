7eb7ec2afaf41a1a9fca3ebd4ac18088cc3be1aa
agusarov <agusarov@bewave.ru>
2021-05-24 19:30:00 +0300

    'init'

---
+++ app/VkService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+class VkService{
+    private function CallApi($id){
+
+    }
+}
---
+++ database/migrations/2021_05_22_104124_create_social_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+ //           $table->unsignedBigInteger('user_id');
+            $table->string('provider_id');
+            $table->string('provider');
+            $table->string('token');
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+}
